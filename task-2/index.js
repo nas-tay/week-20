ee35f94da4cbc406dc7ae666458674bf87a576e5
Anastasia Karzanova <anastasiakarzanova@Anastasias-MacBook-Pro.local>
2022-06-30 16:48:15 +0300

    add task-2

---
+++ task-2/index.js
@@ -0,0 +1,147 @@
+let city = "Moscow";
+let lang = "ru";
+let units = "metric";
+let feelsLike = "Ощущается как: ";
+let humid = "Влажность: ";
+let windTitle = "Ветер: ";
+let speedText = ", со скоростью ";
+let speedMetric = " м/с";
+let maxText = "Макс. t: ";
+let minText = "Мин. t: ";
+let wind = "";
+let windEn = "";
+let errorText = "Город не указан";
+let errorTextEn = "City not specified";
+
+document.addEventListener("DOMContentLoaded", function () {
+    getWeather(city, lang, units);
+});
+
+function getWeather(city, lang, units) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&appid=35918fd762dc1291fb2f2f84ff61dbb7&units=${units}`)
+        .then((response) => response.json())
+        .then((weather) => {
+            let description = weather.weather[0].description;
+            let descriptionUpperCase = description[0].toUpperCase() + description.slice(1);
+            let deg = weather.wind.deg;
+            function getWind(deg) {
+                if (345 < deg <= 15) {
+                    wind = "Западный";
+                    windEn = "West";
+                    return;
+                }
+                if (15 < deg <= 75) {
+                    wind = "Юго-Западный";
+                    windEn = "Southwest";
+                    return;
+                }
+                if (75 < deg <= 105) {
+                    wind = "Южный";
+                    windEn = "South";
+                    return;
+                }
+                if (105 < deg <= 165) {
+                    wind = "Юго-Восточный";
+                    windEn = "Southeast";
+                    return;
+                }
+                if (165 < deg <= 195) {
+                    wind = "Восточный";
+                    windEn = "East";
+                    return;
+                }
+                if (195 < deg <= 255) {
+                    wind = "Северо-Восточный";
+                    windEn = "Northeast";
+                    return;
+                }
+                if (255 < deg <= 285) {
+                    wind = "Северный";
+                    windEn = "North";
+                    return;
+                }
+                if (285 < deg <= 345) {
+                    wind = "Северo-Западный";
+                    windEn = "Northwest";
+                    return;
+                }
+            }
+            document.querySelector(".cityName").innerText = weather.name;
+            document.querySelector(".current").innerText = Math.round(weather.main.temp) + "°";
+            document.querySelector(".feels_like").innerText = feelsLike + Math.round(weather.main.feels_like) + "°";
+            document.querySelector(".humidity").innerText = humid + weather.main.humidity + "%";
+            document.querySelector(".description").innerText = descriptionUpperCase;
+            getWind(deg);
+            document.querySelector(".wind").innerText = windTitle + (lang == "en" ? windEn : wind) + speedText + Math.round(weather.wind.speed) + speedMetric;
+            document.querySelector(".high-low").innerText = maxText + Math.round(weather.main.temp_max) + "°" + " / " + minText + Math.round(weather.main.temp_min) + "°";
+        })
+        .catch((err) => console.log(err));
+}
+
+function chooseCity() {
+    document.querySelector("#error").innerHTML = "";
+    if (document.querySelector("#city").value) {
+        city = document.querySelector("#city").value;
+    } else {
+        document.querySelector("#error").innerHTML = lang == "en" ? errorTextEn : errorText;
+    }
+    getWeather(city, lang, units);
+}
+
+document.querySelector("#lang").addEventListener("change", () => {
+    switch (document.querySelector("#lang").value) {
+        case "ru":
+            lang = "ru";
+            feelsLike = "Ощущается как: ";
+            humid = "Влажность: ";
+            windTitle = "Ветер: ";
+            speedText = ", со скоростью ";
+            speedMetric = "м/с";
+            maxText = "Макс. t: ";
+            minText = "Мин. t: ";
+            document.querySelector(".citySelect").innerHTML = `
+            <label for="city">Введите название города:
+                <input type="text" id="city" placeholder="город"></label>
+            `;
+            if (document.querySelector("#error").innerHTML) {
+                document.querySelector("#error").innerHTML = errorText;
+            }
+            break;
+        case "en":
+            lang = "en";
+            feelsLike = "Feels like: ";
+            humid = "Humidity: ";
+            windTitle = "Wind: ";
+            speedText = ", at a speed of ";
+            speedMetric = " m/s";
+            maxText = "H.: ";
+            minText = "L.: ";
+            document.querySelector(".citySelect").innerHTML = `
+            <label for="city">Enter city name:
+                <input type="text" id="city" placeholder="city"></label>
+            `;
+            if (document.querySelector("#error").innerHTML) {
+                document.querySelector("#error").innerHTML = errorTextEn;
+            }
+            break;
+        default:
+            lang = "ru";
+            break;
+    }
+    getWeather(city, lang, units);
+});
+
+document.querySelector("#units").addEventListener("change", () => {
+    switch (document.querySelector("#units").value) {
+        case "c":
+            units = "metric";
+            break;
+        case "f":
+            units = "imperial";
+            break;
+        default:
+            units = "metric";
+            break;
+    }
+    getWeather(city, lang, units);
+});
